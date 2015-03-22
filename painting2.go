77ae257f0499071bcbb88fa3cf117151552c51d6
wiless <wiless.bytes@gmail.com>
2015-03-23 02:39:08 +0530

    Figure cleaned area,

--- painting2.go
+++ painting2.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 	"time"
@@ -12,18 +14,32 @@ import (
 )
 
 var redraw bool
+var defaultApp appInfo
+var myplot GoPlot
 
+type appInfo struct {
+	Sinewaves float64
+}
+
+func (a *appInfo) UpdateScale(value float64) {
+	a.Sinewaves = value
+	// log.Println("Value udpated to ", a.Sinewaves)
+	myplot.Call("update")
+
+}
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	if err := qml.Run(run); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+
 }
 
 type GoPlot struct {
 	qml.Object
 	Name      string
+	Color     color.RGBA
 	Npoints   int
 	YMax      float64
 	yvals     []float32
@@ -35,21 +51,36 @@ func (g *GoPlot) init() {
 	// width := int32(g.Int("width"))
 	// g.Npoints = g.Int("nPoints")
 
+	// log.Printf("\n Initializing  :  Npoints = %v , Height = %v", int(g.Object.Property("width").(float64)), float32(g.Object.Property("height").(float64)))
+	g.YMax = g.Object.Property("height").(float64) / 2.0
+	zeroline := float32(g.Object.Property("height").(float64)) / 2.0
+	g.Npoints = int(g.Object.Property("width").(float64))
 	g.yvals = make([]float32, g.Npoints)
+	omega := defaultApp.Sinewaves //float64(g.Npoints) /
+	// fmt.Printf("\n frequency = %v", omega)
 	for i := 0; i < g.Npoints; i++ {
-		g.yvals[i] = float32(rand.Int31n(int32(g.YMax)))
+		theta := float64(2.0 * math.Pi * float64(i) * omega / float64(g.Npoints))
+		g.yvals[i] = float32(g.YMax*math.Sin(theta)) + zeroline
+		// g.yvals[i] = float32(i) + zeroline
 	}
 
-	log.Printf("Init() : Seems %#v has only %d points with %v height ", g, g.Npoints, g.YMax)
+	// log.Printf("\n Init() : Seems %#v has only %d points with %v height ", g, g.Npoints, g.YMax)
+	// log.Printf("\n%v*Sine Values(%d) : %v... %v", g.YMax, g.Npoints, g.yvals[0:10], g.yvals[g.Npoints-10:])
 	// g.yvals = vlib.Randsrc(int(g.Npoints), height).ToVectorF()
 
 }
 func (g *GoPlot) Clicked() {
+	g.Update = true
 	log.Printf("\nPlot was clicked  : ")
+
+	g.YMax = g.Object.Property("height").(float64)
+	zeroline := float32(g.Object.Property("height").(float64)) / 2.0
+	g.Npoints = int(g.Object.Property("width").(float64))
+	g.yvals = make([]float32, g.Npoints)
 
 	log.Println("Old sample ", g.yvals[10])
 	for i := 0; i < g.Npoints; i++ {
-		g.yvals[i] = float32(rand.Int31n(int32(g.YMax)))
+		g.yvals[i] = zeroline + (rand.Float32()*float32(g.YMax) - float32(g.YMax)/2.0)
 	}
 	log.Println("New sample ", g.yvals[10])
 
@@ -57,21 +88,34 @@ func (g *GoPlot) Clicked() {
 	// g.mypainter.Call("update")
 }
 
+func toColor4f(c color.RGBA) (r, g, b, a float32) {
+	return float32(c.R) / 256.0, float32(c.G) / 256.0, float32(c.B) / 256.0, float32(c.A) / 256.0
+}
+
 func (g *GoPlot) Paint(p *qml.Painter) {
-	// if g.N == 0 {
+	empty := color.RGBA{}
+	if g.Color == empty {
+		g.Color = color.RGBA{00, 0, 0, 80}
+		// log.Printf("DEFAULT SET TO  %v ", g.Color)
+	} else {
+		log.Println("Custom color ", g.Color)
+	}
+
+	red, green, blue, alpha := toColor4f(g.Color)
 	g.mypainter = p
+	if !g.Update { /// if not initialized even once
+		g.init()
+	}
 
-	log.Printf("\n Painter knows :  %#v \n %#v \n Npoints = %v , Height = %v", g, g.Object)
-
-	g.init()
 	gl := GL.API(p)
-
 	// gl.Enable(GL.BLEND)
 	// gl.BlendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
-	gl.LineWidth(2)
 
 	gl.Begin(GL.QUADS)
-	gl.Color4f(0.1, .1, 0, .4)
+	// gl.Color4f(0.1, .1, 0, .4)
+	// gl.Color4f(00, 0, 0, 0.5)
+	gl.Color4f(red, green, blue, alpha)
+
 	width := float32(g.Float64("width"))
 	height := float32(g.Float64("height"))
 	gl.Vertex2f(0, 0)
@@ -80,13 +124,49 @@ func (g *GoPlot) Paint(p *qml.Painter) {
 	gl.Vertex2f(0, height)
 	gl.End()
 
+	// glColor(solid_color)
+	// glBegin(GL_POLYGON)
+	// glVertex Commands here
+	// glEnd
+	// glDisable(GL_DEPTH_TEST)
+	// glColor(outline_color)
+	// glBegin(GL_LINE_LOOP)
+	// glVertex Commands here
+	// glEnd
+	// glEnable(GL_DEPTH_TEST)
+
+	gl.LineWidth(1)
+
+	/// Draw grid lines
+	gl.LineStipple(1, 0x00ff)
+	gl.Enable(GL.LINE_STIPPLE)
+	gl.Color4f(.8, .8, .8, .3)
+	gl.Begin(GL.LINE_STRIP)
+	gl.Vertex2f(0, float32(g.YMax))
+	gl.Vertex2f(float32(g.Npoints), float32(g.YMax))
+	gl.End()
+	gl.Disable(GL.LINE_STIPPLE)
+
 	gl.Color4f(.5, .5, 0.1, .3)
 	gl.Begin(GL.LINE_STRIP)
+	// gl.Begin(GL.POLYGON)
 	gl.Vertex2f(0, float32(g.yvals[0]))
-	for i := 1; i < int(g.Npoints); i++ {
-		gl.Vertex2f(float32(i)*10, float32(g.yvals[i]))
+	for i := 1; i < len(g.yvals); i++ {
+		gl.Vertex2f(float32(i), float32(g.yvals[i]))
 	}
+
+	// gl.Vertex2f(float32(len(g.yvals))-1, height/2.0)
 	gl.End()
+
+	// gl.Disable(GL.DEPTH_TEST)
+	// gl.Color4f(.5, 0, 0, .2)
+	// gl.Begin(GL.LINE_LOOP)
+	// gl.Vertex2f(0, float32(g.yvals[0]))
+	// for i := 1; i < len(g.yvals); i++ {
+	// 	gl.Vertex2f(float32(i), float32(g.yvals[i]))
+	// }
+	// gl.End()
+	// gl.Enable(GL.DEPTH_TEST)
 
 }
 
@@ -103,6 +183,7 @@ func (g *GoPlot) SetNpoints(npoints int) {
 }
 
 func run() error {
+	defaultApp.Sinewaves = 4.0
 	objs := make([]qml.TypeSpec, 1)
 	objs[0] = qml.TypeSpec{Init: func(r *GoPlot, obj qml.Object) {
 		r.Object = obj
@@ -114,13 +195,16 @@ func run() error {
 	qml.RegisterTypes("GoExtensions", 1, 0, objs)
 
 	engine := qml.NewEngine()
+	ctx := engine.Context()
+	ctx.SetVar("myapp", &defaultApp)
 
 	component, err := engine.LoadFile("plotter.qml")
 	if err != nil {
 		return err
 	}
-
 	win := component.CreateWindow(nil)
+	myplot.Object = win.ObjectByName("myobject")
+
 	win.Set("x", 0)
 	win.Set("y", 0)
 
--- plotter.qml
+++ plotter.qml
@@ -1,7 +1,7 @@
 import QtQuick 2.0
 import GoExtensions 1.0
 import QtQuick.Controls 1.1
-
+import QtQuick.Layouts 1.1
 ApplicationWindow { 
  id:app
  visible: true
@@ -9,35 +9,106 @@ ApplicationWindow {
  title:"Painting Test"
  width:1000
  height:400
-    GoPlot {
-     id:plot1
-     x: 0; y: 0;
-     width: 500 
-     height: 150
-     yMax:50
-  anchors.topMargin: 0
-   anchors.leftMargin: 0
-    update:true
-     
-     objectName:"myobject"
-     npoints:100
+ color: "transparent"
+ ColumnLayout{
+  spacing: 0
+  anchors.fill: parent
+  GoPlot {
+   id:plot1
+   name:"plot1"
+   color:"#A0000000"
+   // x: 10; y: 0;
+   // anchors.fill : parent
 
-     signal mousePressed()
-     MouseArea {
-      id:plotmousearea      
-      anchors.fill: parent
-      }    
-    Component.onCompleted: {
-     console.log("Printing from QML npoints  := ", plot1.npoints) 
-     console.log("Printing from QML npoints  := ", plot1.yMax) 
-     plotmousearea.clicked.connect(mousePressed)
-   }
+   Layout.fillWidth:true
+   Layout.fillHeight:true
+   Layout.alignment:Qt.AlignTop
 
-    // SequentialAnimation on x {
-    //     loops: Animation.Infinite
-    //     NumberAnimation { from: 0; to: 320; duration: 3000; easing.type: Easing.OutQuad}
-    //     NumberAnimation { from: 320; to: 0; duration: 1000; easing.type: Easing.OutQuad}
-    // }
-  
+   // width: parent.width
+   height: 150
+   yMax:50
+   // minimumHeight:100
+   
+   anchors.topMargin: 20
+   anchors.leftMargin: 10
+   anchors.bottomMargin: 50
+   anchors.rightMargin: 10
+
+   objectName:"myobject"
+   npoints:100
+
+   signal mousePressed()
+   MouseArea {
+    id:plotmousearea      
+    anchors.fill: parent
+  }    
+  Component.onCompleted: {
+   console.log("Printing from QML npoints  := ", plot1.npoints) 
+   console.log("Printing from QML npoints  := ", plot1.yMax) 
+   console.log("Printing from QML My Dimensions := ", plot1.height, plot1.width) 
+   plotmousearea.clicked.connect(mousePressed)
+ }
+ // SequentialAnimation on x {
+   //      loops: Animation.Infinite
+   //      NumberAnimation { from: 0; to: 320; duration: 3000; easing.type: Easing.OutQuad}
+   //      NumberAnimation { from: 320; to: 0; duration: 1000; easing.type: Easing.OutQuad}
+   //  }  
+ }
+
+ Rectangle{  
+  color:"#8f1D86FF"
+  height:100
+    // Layout.alignment: Qt.AlignHCenter
+    Layout.fillWidth: true
+    // anchors.topMargin: 55
+    RowLayout{
+
+spacing:10
+    Text    {
+      width:parent.width
+       font.italic: true   
+       baselineOffset:15
+       horizontalAlignment: Text.AlignHCenter
+       verticalAlignment: Text.AlignVCenter
+         wrapMode: Text.Wrap
+      color:"white"; text:"\nFigure showing sine wave. Click on the plot to randomize "
+    }
+    SpinBox {
+    id: spinbox
+    value:myapp.sinewaves
+    minimumValue:.01
+    decimals: 3
+    onEditingFinished:{
+      myapp.updateScale(spinbox.value)  
+    }
+
+    
 }
+TextField { text: "Hello"; font.capitalization: Font.AllLowercase ;placeholderText:"Type anything here"}
+ TextArea {
+      // width:parent.width
+       font.italic: true   
+       baselineOffset:15
+       horizontalAlignment: Text.AlignHCenter
+     
+      
+    }
+
+    }
+ } 
+ // Label {
+
+ //   id:figtitle
+ //   Layout.alignment: Qt.AlignHCenter
+ //   Layout.fillHeight: false
+ //   // font.pixelSize: 10
+ //   font.bold:true
+ //   color: "steelblue" // steelblue
+ //   text:"Figure : Plot Rendered by Golang  "
+
+ // }
+
+}
+
+
 }
